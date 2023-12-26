9fb0f125871b029bdc1e6bb1fd31aab395eb9fc0
yizhenpong <yizhenpong@gmail.com>
2023-12-27 02:46:10 +0800

    update forum/models.py

--- PKUmoocServer/forum/models.py
+++ PKUmoocServer/forum/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+from user_info.models import User
+# from django.contrib.auth.models import User
 
 # Create your models here.
+class Forum(models.Model):
+    name = models.CharField(max_length=100, verbose_name="讨论区")
+
+class ForumSection(models.Model):
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE, verbose_name="板块")
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    section = models.ForeignKey(ForumSection, on_delete=models.CASCADE, verbose_name="帖子")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now,verbose_name="发布时间")
+    class Meta:
+        ordering = ['-created_at']
+    def __str__(self):
+        return self.content
+
+class Reply(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name="回复")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now, verbose_name="发布时间")
+    class Meta:
+        ordering = ['-created_at']
+    def __str__(self):
+        return self.content
