5b5d0bdef14b87556d0a007f12fe2697219aa175
yizhenpong <yizhenpong@gmail.com>
2023-12-27 02:46:22 +0800

    update forum/admin.py

--- PKUmoocServer/forum/admin.py
+++ PKUmoocServer/forum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from forum.models import DiscussionForum, ForumSection, Post, Reply
 
 # Register your models here.
+admin.site.register(DiscussionForum)
+admin.site.register(ForumSection)
+admin.site.register(Post)
+admin.site.register(Reply)
