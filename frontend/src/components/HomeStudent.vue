d5211957619157131828d07c2e04e49016e0206d
yszaqdn <yuxiang123@stu.pku.edu.cn>
2023-12-27 19:48:51 +0800

    update student home

--- frontend/src/components/HomeStudent.vue
+++ frontend/src/components/HomeStudent.vue
@@ -1,15 +1,28 @@
-
-
 <template>
-    <div>
-      <div v-for="course in courses" :key="course.id">
-        <h2>{{ course.title }}</h2>
-        <p>{{ course.year }} {{ course.session }}</p>
-        <p>Teachers: {{ course.teachers.join(', ') }}</p>
-        <p>Students: {{ course.students.join(', ') }}</p>
-      </div>
+    <div class="course-box">
+        <div v-for="course in courses" :key="course.id" class="courses">
+            <div class="course-info">
+                <div class="tag">
+                    {{ course.year }}
+                </div>
+                <div class="tag">
+                    {{ course.session }}
+                </div>
+                <div class="tag">
+                    Teachers: {{ course.teachers.join(', ') }}
+                </div>
+                
+            </div>
+            <div class="course_title">
+                {{ course.title }}
+            </div>
+
+
+
+
+        </div>
     </div>
-  </template>
+</template>
 
 <script>
 import axios from 'axios'
@@ -24,7 +37,7 @@ export default {
         let IDnum = localStorage.getItem('IDnum');
         let token = localStorage.getItem('token');
         try {
-            const response = await axios.get('/api/course/',{
+            const response = await axios.get('/api/course/', {
                 headers: {
                     'Authorization': `Bearer ${token}`
                 }
@@ -40,3 +53,40 @@ export default {
 }
 
 </script>
+
+<style>
+.course-box {
+    position: absolute;
+    top: 50px;
+    left: 20px;
+    border: 2px solid black;
+    border-radius: 5px;
+    width:40%;
+    
+}
+.courses {
+    padding: 10px;
+}
+
+.course_title {
+    font-size: large;
+    font-weight: bolder;
+    font-family: Georgia, Arial, sans-serif;
+    color: black;
+    text-decoration: none;
+    padding: 5px 5px 5px 0;
+}
+
+.course-info {
+    display: flex;
+}
+
+.tag {
+    padding: 2px 5px 2px 5px;
+    margin: 5px 5px 5px 0;
+    font-family: Georgia, Arial, sans-serif;
+    font-size: small;
+    background-color: #4e4e4e;
+    color: whitesmoke;
+    border-radius: 5px;
+}</style>
